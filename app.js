edbf84be79a2b6e40f545a9cd8c7ce1cb810b208
Rahul Sharma <rahu8299@gmail.com>
2020-07-18 14:55:49 +0530

    completing basic template

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ app.listen(8800);
 app.get('/', (req,res)=>{
    res.sendFile("./index.html", {root: __dirname})
 });
+// let TOKEN = "de6cee09a2f30c7223b7c616994c4fc20a7c2929";
+
+// var client = Owlbot(TOKEN);
+// //    client.define('owl').then(function(result){
+// //       console.log(result);
+// //       res.
+// //    }); 
